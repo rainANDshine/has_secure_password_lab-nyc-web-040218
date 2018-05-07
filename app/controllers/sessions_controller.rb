84e41e765cc44851ba369f79fbfe5110fe394d7a
Shun Yao <ysmiracle@hotmail.com>
2018-05-07 04:43:40 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,29 +2,14 @@ class SessionsController < ApplicationController
   def new
   end
 
-  # def create
-  #   byebug
-  #   @user = User.find_by(name: params[:name])
-  #   if @user && @user.authenticate(params[:password])
-  #     session[:user_id] = @user.id
-  #     redirect_to welcome_path
-  #   else
-  #     redirect_to login_path
-  #   end
-  # end
-
   def create
-   user = User.find_by(name: params[:name])
-
-   user = user.try(:authenticate, params[:password])
-
-   return redirect_to(controller: 'sessions', action: 'new') unless user
-
-   session[:user_id] = user.id
-
-   #@user = user
-
-   redirect_to controller: 'welcome', action: 'show'
- end
-
+    byebug
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to welcome_path
+    else
+      redirect_to login_path
+    end
+  end
 end
