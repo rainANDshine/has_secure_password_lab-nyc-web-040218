dfed847b6ea54ddda5823b3c0f81ffdc8998ee33
Shun Yao <ysmiracle@hotmail.com>
2018-05-07 04:12:01 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(name: params[:name])
     if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       session[:user_name] = @user.name
       redirect_to welcome_path
     else
